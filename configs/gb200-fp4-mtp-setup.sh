68417975102e5149b23245a04fd4300d90d47bf0
Baizhou Zhang <sobereddiezhang@gmail.com>
2026-01-06 22:23:16 +0800

    Update mtp recipes to 0.5.7 (#74)

--- configs/gb200-fp4-mtp-setup.sh
+++ configs/gb200-fp4-mtp-setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-BRANCH="baizhou/gb200-spec"
+BRANCH="gb200-spec"
 
 cd /sgl-workspace/sglang
 git remote remove origin
 git remote add origin https://github.com/sgl-project/sglang.git
 git fetch origin
 git checkout origin/${BRANCH}
+pip install mooncake-transfer-engine==0.3.7.post2
